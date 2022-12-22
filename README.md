e321415e4ce8bade94da74959a09c0e0d6d1bfe7
Matthew Gebhart <Matthewgebhart@gmail.com>
2022-12-21 23:57:19 -0800

    everything done and all tests passing just with short sample text for now

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-.venv
+# LAB - Class 03
+## Project: File IO and Exceptions
+### Author: Matthew Gebhart
+### Links and Resources
+back-end server url (when applicable)
+front-end application (when applicable)
+### Setup
+.env requirements (where applicable)
+- Python 3.11 .venv environment
+
+How to initialize/run your application (where applicable)
+python madlib.py
+
+Tests
+How do you run tests?
+from root folder - pytest
+
+
+Describe any tests that you did not complete, skipped, etc
+- all tests passing
+
+### credit to my classmate Tracy Oakley for help with the parse function
--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,1 +1,69 @@
-print("Proof of life")
+
+def welcome():
+    print("""
+*********************************
+Welcome to MadLibs! 
+To play, please {enter} the 
+{correct} word type when prompted.
+Have {farts}!
+*********************************
+""")
+
+example = '../assets/dark_and_stormy_night_template.txt'
+example2 = '../assets/make_me_a_video_game.txt'
+
+def read_template(txt_file):
+    try:
+        with open(txt_file, 'r') as f:
+            contents = f.read()
+        return contents
+    except FileNotFoundError as not_found:
+        raise not_found
+
+
+# print(read_template('../assets/dark_and_stormy_night_template.txt'))
+
+def parse_template(start_string):
+    stripped_string = start_string.replace("Adjective", "").replace("Noun", "")
+
+    pieces_string = start_string
+    pieces = []
+
+    while pieces_string.find("{") != -1:
+        slice_open = pieces_string.find("{")
+        slice_close = pieces_string.find("}")
+        pieces.append(pieces_string[slice_open+1:slice_close])
+        pieces_string = pieces_string[:slice_open] + pieces_string[slice_close+1:]
+    # print(pieces)
+    pieces = tuple(pieces)
+
+    # print(stripped_string)
+    # print(pieces)
+    return (stripped_string, pieces)
+
+
+# parse_template(read_template(example))
+
+def merge(empty_string, new_inputs):
+    new_lib = empty_string.format(*new_inputs)
+    return new_lib
+
+
+if __name__ == "__main__":
+    welcome()
+    collected_answers = []
+    (stripped_string, pieces) = parse_template(read_template(example))
+
+    for x in pieces:
+        print(f"supply \"{x}\" ")
+        answer = input("> ")
+        collected_answers.append(answer)
+
+    collected_answers = tuple(collected_answers)
+
+    print(f"""
+    your new madlib:
+    {merge(stripped_string, collected_answers)}
+    
+    Thanks for playing!
+    """)
